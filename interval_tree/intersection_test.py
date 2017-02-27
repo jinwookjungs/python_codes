8cfa618e28983e7dd119ece67a64189dd5522615
Jinwook Jung <jinwookjungs@gmail.com>
2017-02-27 12:11:46 -0500

    Fixed some minor bugs.

--- interval_tree/intersection_test.py
+++ interval_tree/intersection_test.py
@@ -2,7 +2,7 @@
     File name      : intersection_test.py
     Author         : Jinwook Jung
     Created on     : Fri Feb 24 20:27:42 2017
-    Last modified  : 2017-02-26 00:30:29
+    Last modified  : 2017-02-27 12:10:56
     Python version : 
 '''
 
@@ -10,6 +10,7 @@ import sys
 from intervaltree import Interval, IntervalTree
 from copy import deepcopy
 from functools import total_ordering
+from collections import OrderedDict
 
 
 @total_ordering
@@ -21,19 +22,19 @@ class Rectangle(object):
 
     @property
     def width(self):
-        return self.urx - self.llx
-
+        return self.urx-self.llx
 
     @property
     def height(self):
-        return self.ury - self.lly
-
+        return self.ury-self.lly
 
     @property
     def area(self):
         return self.width * self.height
+   
+    def __repr__(self):
+        return 'Rectangle({0.llx!r}, {0.lly!r}, {0.urx!r}, {0.ury!r})'.format(self)
 
-    
     def __eq__(self, other):
         return (self.llx, self.lly, self.urx, self.ury) \
                 == (other.llx, other.lly, other.urx, other.ury)
@@ -42,36 +43,69 @@ class Rectangle(object):
         return self.area < other.area
 
 
+@total_ordering
 class PinReach(object):
     def __init__(self, name, llx, lly, urx, ury):
-        """ A diamond shape defining reachable region from a pin. """
+        """ A pin reach. """
         self.name = name
-        self.llx, self.lly = llx, lly
-        self.urx, self.ury = urx, ury
+        self.bbox = Rectangle(llx, lly, urx, ury)
+    
+    @property
+    def llx(self):
+        return self.bbox.llx
 
+    @property
+    def lly(self):
+        return self.bbox.lly
+
+    @property
+    def urx(self):
+        return self.bbox.urx
+
+    @property
+    def ury(self):
+        return self.bbox.ury
+
+    @property
+    def width(self):
+        return self.urx-self.llx
+
+    @property
+    def height(self):
+        return self.ury-self.lly
+
+    def __repr__(self):
+        return 'PinReach({0.name!r}, {0.bbox!r})'.format(self)
 
     def __str__(self):
         return "%s: (%d,%d), (%d,%d)" \
                 % (self.name, self.llx, self.lly, self.urx, self.ury)
 
-
     def __eq__(self, other):
-        return self.name == other.name
-
+        return self.name == other.name and self.bbox == other.bbox
+    
+    def __lt__(self, other):
+        return self.bbox < other.bbox
 
     def __hash__(self):
         return hash(self.name)
 
 
 class Bound(object):
-    def __init__(self, sink, coord, is_left=True):
+    def __init__(self, sinks, coord, direction='l'):
         """ lower/upper bound of a pin reach. """
-        self.sink = sink
+        self.sinks = sinks      # A set of sinks
         self.coord = coord
-        self.is_left = is_left      # True if it represents a left boundary
+        self.direction = direction      # 'l' or 'r'
+        self.name = '/'.join([s.name for s in sinks])
 
-        self.name = sink.name
+    @property
+    def is_left(self):
+        return True if self.direction == 'l' else False
 
+    @property
+    def sink_name_set(self):
+        return {s.name for s in self.sinks}
 
     def __str__(self):
         return "%s " % (self.__class__.__name__) \
@@ -79,63 +113,85 @@ class Bound(object):
                + "%s)" % ("Left" if self.is_left else "Right")
 
 
-class Level(object):
+class BoundPool(object):
     def __init__(self):
-        self.nodes = list()
+        """ A set of bounds. """
+        self.bound_dict = dict()    # Key: (coord, 'l'/'r'), Value: sink list
+
+    def add_bound(self, sink, coord, direction='l'):
+        print("\t%s - %d - %s" % (sink, coord, direction))
+        assert direction in ('l', 'r')
+        try:
+            self.bound_dict[coord,direction].append(sink)
+        except KeyError:
+            self.bound_dict[coord,direction] = list()
+            self.bound_dict[coord,direction].append(sink)
+
+    def get_sorted_bounds(self):
+        d = OrderedDict(sorted(self.bound_dict.items(), key=lambda t:t[0]))
+        return [Bound(v,k[0],k[1]) for k,v in d.items()]
+
+    def __len__(self):
+        return len(self.bound_dict)
+
+    def __iter__(self):
+        return iter(self.bound_dict.items())
 
 
-class Node(object):
+class Node(Rectangle):
     def __init__(self, sink_set, llx):
         self.sink_dict = {s.name : s for s in sink_set}
 
         self.llx, self.urx = llx, None
         self.lly, self.ury = None, None
-
     
     def add_sink(self, s):
         self.sink_dict[s.name] = s
-
     
-    def remove_sink_by_name(self, name):
+    def remove_sink_by_name(self, name_set):
         try:
-            del self.sink_dict[name]         
+            for name in name_set:
+                del self.sink_dict[name]
         except KeyError as ke:
-            sys.stderr.write("%r" % ke)
+            sys.stderr.write("(E in Node) %r" % ke)
             sys.stderr.write("\n")
 
-    def get_area(self):
+    @property
+    def area(self):
         try:
             return (self.urx-self.llx) * (self.ury-self.lly)
         except TypeError:
             return 0 
 
+    def __repr__(self):
+        name = '/'.join([s.name for s in self.sink_dict.values()])
+    
+        return "%s (%r, %r) (%r, %r) area=%r" \
+                % (name, self.llx, self.lly, self.urx, self.ury, self.area)
 
     def __str__(self):
         name = '/'.join([s.name for s in self.sink_dict.values()])
-        area = self.get_area()
     
         return "%s (%r, %r) (%r, %r) area=%r" \
-                % (name, self.llx, self.lly, self.urx, self.ury, area)
-
+                % (name, self.llx, self.lly, self.urx, self.ury, self.area)
 
     def __hash__(self):
         names = ''.join(self.sink_dict.keys())
         return hash(names)
 
-
     def __eq__(self, other):
         return self.__hash__() == other.__hash__()
+    
+    def __iter__(self):
+        return iter(self.sink_dict.values())
 
 
-
-class NodeSet(object):
+class NodePool(object):
     def __init__(self):
         self.node_set = set()
        
-
     def add_node(self, n):
         self.node_set.add(n)
-
 
     def remove_node(self, n):
         try:
@@ -144,11 +200,19 @@ class NodeSet(object):
             sys.stderr.write("%r" % ke)
             sys.stderr.write("\n")
 
-
     def get_nodes_by_sink_name(self, name):
         """ Return nodes associated with the sink. """
-        return [v for v in self.node_set if name in v.sink_dict.keys()]
+        print(name)
+        for v in self.node_set:
+            print(set(v.sink_dict.keys()))
+            print(name.intersection(set(v.sink_dict.keys())))
+        print("")
+        return [v for v in self.node_set if len(name.intersection(set(v.sink_dict.keys())))!=0]
 
+    def get_nodes_sorted(self):
+        return sorted(self.node_set, 
+                      key=lambda t: (len(t.sink_dict), t.area), 
+                      reverse=True)
 
     def __iter__(self):
         return iter(self.node_set)
@@ -162,9 +226,10 @@ class Checker(object):
         self.T_x = IntervalTree()
         self.T_y = IntervalTree()
 
-        self.x_bounds, self.y_bounds = list(), list()
-        self.solution = None
+        self.x_bounds, self.y_bounds = BoundPool(), BoundPool()
 
+        self.nodes = None
+        self.selected = None
 
     def read_input(self, file_name):
         # Read file        
@@ -191,36 +256,36 @@ class Checker(object):
         src_llx, src_lly = self.source.llx, self.source.lly
         src_urx, src_ury = self.source.urx, self.source.ury
 
+        print("Generating bounds.")
         for s in self.sinks:
             # I don't need bound coordinates that are either on the left or
             # on the right of the source
+            print("%r - X" % (s))
             if s.llx > src_llx and s.llx < src_urx:
-                self.x_bounds.append(Bound(s, s.llx, is_left=True))
+                self.x_bounds.add_bound(s, s.llx, 'l')
 
             if s.urx > src_llx and s.urx < src_urx:
-                self.x_bounds.append(Bound(s, s.urx, is_left=False))
+                self.x_bounds.add_bound(s, s.urx, 'r')
 
             # Y bounds
+            print("%r - Y" % (s))
             if s.lly > src_lly and s.lly < src_ury:
-                self.y_bounds.append(Bound(s, s.lly, is_left=True))
+                self.y_bounds.add_bound(s, s.lly, 'l')
 
             if s.ury > src_lly and s.ury < src_ury:
-                self.y_bounds.append(Bound(s, s.ury, is_left=False))
-                
-        self.x_bounds = sorted(self.x_bounds, key=lambda b: (b.coord, b.sink.urx))
-        self.y_bounds = sorted(self.y_bounds, key=lambda b: (b.coord, b.sink.ury))
-
+                self.y_bounds.add_bound(s, s.ury, 'r')
+               
+        print("")
         print("#Sinks : %d" % len(self.sinks))
         print("#Bounds : %d/%d" % (len(self.x_bounds), len(self.y_bounds)))
         print("")
 
-        for b in self.x_bounds:
+        for b in self.x_bounds.get_sorted_bounds():
             print(b)
         print("")
-        for b in self.y_bounds:
+        for b in self.y_bounds.get_sorted_bounds():
             print(b)
         print("")
-
 
     def initialize_interval_trees(self):
         for s in self.sinks:
@@ -234,7 +299,6 @@ class Checker(object):
 #        for i in self.T_y:
 #            print(i)
 #        print("")
-
     
     def generate_nodes(self):
         print("-"*79)
@@ -242,8 +306,8 @@ class Checker(object):
         print("-"*79)
 
         crossed = list()
-        active_nodes = NodeSet()
-        Nx = NodeSet()
+        active_nodes = NodePool()
+        Nx = NodePool()
 
         #---------------------------------------
         # X-direction
@@ -262,17 +326,21 @@ class Checker(object):
         print("")
 
         # Now, start searching toward x-direction
-        for b in self.x_bounds:
+        for b in self.x_bounds.get_sorted_bounds():
             if b.is_left is False:
                 # We meet the right boundary of a sink now.
                 # Find the active nodes having the sink.
-                nodes = active_nodes.get_nodes_by_sink_name(b.name)
+                print("NAME:")
+                print(b.name)
+                nodes = active_nodes.get_nodes_by_sink_name(b.sink_name_set)
 
                 for n in nodes:
                     n_new = deepcopy(n)
-                    n_new.remove_sink_by_name(b.name)
+                    n_new.remove_sink_by_name(b.sink_name_set)
                     
-                    active_nodes.add_node(n_new)
+                    if len(n_new.sink_dict) != 0:
+                        active_nodes.add_node(n_new)
+
                     active_nodes.remove_node(n)
 
                     # Update its urx
@@ -285,15 +353,13 @@ class Checker(object):
                 new_nodes = list()
                 for n in active_nodes.node_set:
                     n_new = deepcopy(n)
-                    n_new.add_sink(b.sink)
+                    [n_new.add_sink(s) for s in b.sinks]
                     n_new.llx = b.coord
                     new_nodes.append(n_new)
 
                 # If there's no active node, create one
                 if len(active_nodes.node_set) == 0:
-                    n_new = Node()
-                    n_new.add_sink(b.sink)
-                    n_new.llx = b.coord
+                    n_new = Node(b.sinks, b.coord)
                     new_nodes.append(n_new)
 
                 [active_nodes.add_node(n) for n in new_nodes]
@@ -312,15 +378,15 @@ class Checker(object):
         # Y-direction
         #---------------------------------------
         crossed_sink_names = list()
-        active_nodes = NodeSet()
-        Ny = NodeSet()
+        active_nodes = NodePool()
+        Ny = NodePool()
 
         # 0. Find intersection with the left-bound of the source
         for i in self.T_y[self.source.lly]:
             crossed_sink_names.append(i.data.name)
 
         for n in Nx.node_set:
-            sink_names = {n.name for n in n.sink_dict.values()}
+            sink_names = {sink.name for sink in n}
 
             if sink_names.issubset(crossed_sink_names):
                 n.lly = self.source.lly
@@ -329,15 +395,20 @@ class Checker(object):
         [Nx.remove_node(n) for n in active_nodes.node_set]
 
         # 1. Search toward y-direction
-        for b in self.y_bounds:
+        for b in self.y_bounds.get_sorted_bounds():
+            print(b)
+            print(b.sink_name_set)
             if b.is_left is False:  
-                # We meet the right boundary a sink
-                # Close every node having the sink
-                nodes = active_nodes.get_nodes_by_sink_name(b.name)
+                # We meet the right boundary of a sink.
+                # Close every node having the sink.
+                nodes = active_nodes.get_nodes_by_sink_name(b.sink_name_set)
+                print(nodes)
 
                 for n in nodes:
+                    print(n)
                     n_new = deepcopy(n)
-                    n_new.remove_sink_by_name(b.name)
+                    n_new.remove_sink_by_name(b.sink_name_set)
+                    print(n_new)
 
                     if len(n_new.sink_dict) != 0:
                         active_nodes.add_node(n_new)
@@ -346,8 +417,8 @@ class Checker(object):
                     n.ury = b.coord
                     Ny.add_node(n)
 
-                # Remove the sink as well in the nodes not inactivate currently
-                nodes = Nx.get_nodes_by_sink_name(b.name)
+                # Remove the sink as well in the nodes not inactivted currently
+                nodes = Nx.get_nodes_by_sink_name(b.sink_name_set)
                 for n in nodes:
                     n.sink_dict = {k:v for k,v in n.sink_dict.items() 
                                        if k != b.name}
@@ -364,7 +435,7 @@ class Checker(object):
                 for i in self.T_y[b.coord]:
                     crossed_sink_names.append(i.data.name)
 
-                for n in Nx.node_dict.values():
+                for n in Nx:
                     names = set(n.sink_dict.keys())
                     if names.issubset(crossed_sink_names):
                         n.lly = self.source.lly
@@ -375,45 +446,66 @@ class Checker(object):
             n.ury = self.source.ury
             Ny.add_node(n)
 
+        self.nodes = Ny.get_nodes_sorted()
         print("")
-        [print("\t%s" % (n)) for n in Ny]
+        [print("\t%s" % (n)) for n in self.nodes]
 
-        self.solution = Ny
+    def select_nodes(self):
+        sink_names = {s.name for s in self.sinks}
+        candidates = []
 
-
-    def plot_solution(self):
+    def plot_generated_nodes(self):
         import matplotlib.cm as cm
         import matplotlib.pyplot as plt
         from matplotlib.collections import PatchCollection
         from matplotlib.patches import Rectangle
         from numpy import array as np_array
 
-        patches = list()
         areas = list()
         
-        x_max, y_max = 0,0
+        fig = plt.figure()
+        fig.set_size_inches(15,20)
+        ax1 = fig.add_subplot(211)
 
-        for n in self.solution.node_set:
+        xs = [(s.llx, s.urx) for s in self.sinks + [self.source]]
+        ys = [(s.lly, s.ury) for s in self.sinks + [self.source]]
+
+        x_max, y_max = max([x[1] for x in xs]), max([y[1] for y in ys])
+        x_min, y_min = min([x[0] for x in xs]), min([y[0] for y in ys])
+
+        for s in self.sinks:
+            x,y = s.llx, s.lly
+            w,h = s.width, s.height
+            rectangle = Rectangle((x,y), w, h, alpha=0.3)
+            ax1.add_patch(rectangle)
+
+        x,y = self.source.llx, self.source.lly
+        w,h = self.source.width, self.source.height
+        rectangle = Rectangle((x,y), w, h, facecolor='red', alpha=0.3)
+        ax1.add_patch(rectangle)
+
+        ax1.set_xlim([x_min,x_max])
+        ax1.set_ylim([x_min,y_max])
+
+        patches = list()
+        for n in self.nodes:
             x,y = n.llx, n.lly
-            w,h = n.urx-n.llx, n.ury-n.lly
+            w,h = n.width, n.height
             rectangle = Rectangle((x,y), w, h)
             patches.append(rectangle)
-            areas.append(n.get_area())
+            areas.append(n.area)
 
-            x_max = n.urx if n.urx > x_max else x_max
-            y_max = n.ury if n.ury > y_max else y_max
 
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
+        ax2 = fig.add_subplot(212)
         col = PatchCollection(patches, alpha=0.4)
         col.set(array=np_array(areas), cmap='jet')
 
-        ax.add_collection(col)
-        ax.set_xlim([0,x_max])
-        ax.set_ylim([0,y_max])
-        plt.colorbar(col)
+        ax2.add_collection(col)
+        ax2.set_xlim([x_min,x_max])
+        ax2.set_ylim([x_min,y_max])
+        # plt.colorbar(col)
 
-        fig.savefig('test.png')
+        fig.savefig('test.png', dpi=200)
 
 
 if __name__ == '__main__':
@@ -428,5 +520,5 @@ if __name__ == '__main__':
     checker.initialize_interval_trees()
 
     checker.generate_nodes()
-    checker.plot_solution()
-
+    checker.select_nodes()
+    checker.plot_generated_nodes()
